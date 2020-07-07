a035e7bb947b3a5b0e739a1e30a12d25b666598f
OchirnyamB <ochirnyam@gmail.com>
2020-07-06 20:39:58 -0400

    Rework perceptron code snippets

--- binaryClassifier/perceptron/perceptron.py
+++ binaryClassifier/perceptron/perceptron.py
@@ -8,11 +8,10 @@ np.random.seed(0)
 # N-inputs - 1 hidden layer with 1 neuron - Output Layer
 class Perceptron:
     # The value of alpha controls how large or small of a step we take in gradient descent
-    def __init__(self, N, bias, alpha=0.1):
+    def __init__(self, N, alpha=0.1):
         # Initialize the weight matrix with random values sampled from "normal"Gaussian disribution with 0 mean and unit variance
         # Then scaling the weight matrix
-        self.W = np.random.rand(N)/np.sqrt(N)
-        self.bias = bias
+        self.W = np.random.rand(N+1)/np.sqrt(N)
         self.alpha = alpha
 
     def step(self, x):
@@ -21,11 +20,12 @@ class Perceptron:
 
     # Training procedure function
     def fit(self, X, y, epochs):
+        X=np.c_[X, np.ones((X.shape[0]))]
         # Loop over the desired number of epochs
         for epoch in np.arange(0, epochs):
             # Loop over each individual data point
             for(x, target) in zip(X, y):
-                pred = self.step(np.dot(x, self.W)+self.bias)
+                pred = self.step(np.dot(x, self.W))
                 
                 # Only perform a weight update if our prediction does not match the target
                 if(pred != target):
@@ -37,4 +37,5 @@ class Perceptron:
     def predict(self, X):
         # Ensure that our input is a matrix
         X=np.atleast_2d(X)
+        X=np.c_[X, np.ones((X.shape[0]))]
         return self.step(np.dot(X, self.W))
